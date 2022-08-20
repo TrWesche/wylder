02c0c2e7ca7f205af5879181f6bdd6ca5ec1b846
TrWesche <TrWesche@live.com>
2022-08-20 14:01:34 -0400

    Added Key Mouse Gamepad mappings based on glfw values

---
+++ Wylder/src/Wylder/GUI/MouseCodes.h
@@ -0,0 +1,21 @@
+#pragma once
+/*! @defgroup buttons Mouse buttons
+*  @brief Mouse button IDs.
+*
+*  See [mouse button input](@ref input_mouse_button) for how these are used.
+*
+*  @ingroup input
+*  @{ */
+#define WY_MOUSE_BUTTON_1         0
+#define WY_MOUSE_BUTTON_2         1
+#define WY_MOUSE_BUTTON_3         2
+#define WY_MOUSE_BUTTON_4         3
+#define WY_MOUSE_BUTTON_5         4
+#define WY_MOUSE_BUTTON_6         5
+#define WY_MOUSE_BUTTON_7         6
+#define WY_MOUSE_BUTTON_8         7
+#define WY_MOUSE_BUTTON_LAST      WY_MOUSE_BUTTON_8
+#define WY_MOUSE_BUTTON_LEFT      WY_MOUSE_BUTTON_1
+#define WY_MOUSE_BUTTON_RIGHT     WY_MOUSE_BUTTON_2
+#define WY_MOUSE_BUTTON_MIDDLE    WY_MOUSE_BUTTON_3
+/*! @} */
